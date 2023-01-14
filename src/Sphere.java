9e8850ba169d58209cfd7b432c7c8d7179e1153d
Radclyf <shanksakagame66@gmail.com>
2023-01-14 13:13:47 +0800

    Milestone C

--- src/Sphere.java
+++ src/Sphere.java
@@ -1,7 +1,7 @@
 public class Sphere extends Figure {
     private double radius;
 
-    public Sphere(double radius) {
+    public Sphere(double radius, double r2, double r3) {
         this.radius = radius;
     }
 
@@ -12,6 +12,23 @@ public class Sphere extends Figure {
 
     @Override
     double getVolume() {
-        return (4/3) * Math.PI * Math.pow(radius, 3);
+        return (4 / 3) * Math.PI * Math.pow(radius, 3);
+    }
+
+    @Override
+    public String toString() {
+        return "Sphere (surface area: " + getSurfaceArea() + ", volume: " + getVolume() + ")";
+    }
+
+    public static class Ellipsoid extends Sphere {
+        private double r1;
+        private double r2;
+        private double r3;
+        public Ellipsoid(double r1, double r2, double r3) {
+            super(r1,r2,r3);
+            this.r1 = r1;
+            this.r2 = r2;
+            this.r3 = r3;
+        }
     }
 }
